b179cd4c2f8c5c97781c0b54e7cee2e4f6271c74
Irina Litvinova <neforsha@yandex.ru>
2024-06-20 13:55:16 +0700

    feat: added env

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { USER_LOCALSTORAGE_KEY } from '../const/localStorage';
 
 export const $api = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY),
     },
